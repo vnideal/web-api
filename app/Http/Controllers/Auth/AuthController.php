1745073eb90a1e8e0ae9b842b0dbd88d87f1c4d9
nhannguyen <a4rum.vn@gmail.com>
2020-12-27 15:44:18 +0700

    add ApiController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Biz\UserBiz;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\Models\User;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\Passport;
 
-class AuthController extends Controller
+class AuthController extends ApiController
 {
     use ApiResponser;
 
